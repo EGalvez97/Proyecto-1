31fd73cb7b12cb39b5b4c27099bf564ed08b4781
Luys Mendoza <emmanuel_luis9@hotmail.com>
2022-05-25 23:15:58 -0600

    login interfaz

--- Proyecto/Login.Designer.cs
+++ Proyecto/Login.Designer.cs
@@ -34,12 +34,14 @@
             this.btn_Cancelar = new System.Windows.Forms.Button();
             this.txt_Usuario = new System.Windows.Forms.TextBox();
             this.txt_Contraseña = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(294, 280);
+            this.label1.Location = new System.Drawing.Point(263, 335);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(47, 15);
             this.label1.TabIndex = 0;
@@ -48,7 +50,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(303, 409);
+            this.label2.Location = new System.Drawing.Point(263, 450);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 15);
             this.label2.TabIndex = 1;
@@ -56,7 +58,7 @@
             // 
             // btn_Iniciar
             // 
-            this.btn_Iniciar.Location = new System.Drawing.Point(223, 540);
+            this.btn_Iniciar.Location = new System.Drawing.Point(173, 575);
             this.btn_Iniciar.Name = "btn_Iniciar";
             this.btn_Iniciar.Size = new System.Drawing.Size(108, 42);
             this.btn_Iniciar.TabIndex = 2;
@@ -67,7 +69,7 @@
             // btn_Cancelar
             // 
             this.btn_Cancelar.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_Cancelar.Location = new System.Drawing.Point(354, 540);
+            this.btn_Cancelar.Location = new System.Drawing.Point(296, 575);
             this.btn_Cancelar.Name = "btn_Cancelar";
             this.btn_Cancelar.Size = new System.Drawing.Size(108, 42);
             this.btn_Cancelar.TabIndex = 3;
@@ -79,27 +81,40 @@
             // 
             this.txt_Usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_Usuario.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txt_Usuario.Location = new System.Drawing.Point(223, 332);
+            this.txt_Usuario.Location = new System.Drawing.Point(173, 377);
             this.txt_Usuario.Name = "txt_Usuario";
-            this.txt_Usuario.Size = new System.Drawing.Size(193, 22);
+            this.txt_Usuario.Size = new System.Drawing.Size(231, 22);
             this.txt_Usuario.TabIndex = 4;
             // 
             // txt_Contraseña
             // 
             this.txt_Contraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txt_Contraseña.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txt_Contraseña.Location = new System.Drawing.Point(223, 447);
+            this.txt_Contraseña.Location = new System.Drawing.Point(173, 501);
             this.txt_Contraseña.Name = "txt_Contraseña";
-            this.txt_Contraseña.Size = new System.Drawing.Size(193, 22);
+            this.txt_Contraseña.Size = new System.Drawing.Size(231, 22);
             this.txt_Contraseña.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::Proyecto.Properties.Resources.administrador;
+            this.pictureBox1.Location = new System.Drawing.Point(173, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(231, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Aquamarine;
-            this.ClientSize = new System.Drawing.Size(684, 761);
+            this.BackgroundImage = global::Proyecto.Properties.Resources.fondoImagen__1_;
+            this.ClientSize = new System.Drawing.Size(596, 688);
             this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txt_Contraseña);
             this.Controls.Add(this.txt_Usuario);
             this.Controls.Add(this.btn_Cancelar);
@@ -109,6 +124,7 @@
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,5 +138,6 @@
         private Button btn_Cancelar;
         private TextBox txt_Usuario;
         private TextBox txt_Contraseña;
+        private PictureBox pictureBox1;
     }
 }
